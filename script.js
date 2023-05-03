b3dae950268928f03625d8b5f022916e4b3ad21f
Jenny Robbins <jenny.robbins@softwire.com>
2023-05-03 17:36:39 +0100

    better gifs

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ numbersTable.addEventListener('click', function(event){
 
     fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=numbers`).then(async function (response) {
         const gif = await response.json()
-        const gifElement = document.getElementById("gif-section")
-        gifElement.innerHTML +=`<iframe src=${gif.data.embed_url}>`
+        const gifElement = document.createElement('span')
+        gifElement.innerHTML =`<iframe src=${gif.data.embed_url}>`
+        const gifSection = document.getElementById("gif-section")
+        gifSection.appendChild(gifElement)
+        console.log(gif.data.embed_url)
     });
 })
 

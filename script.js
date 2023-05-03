f23e6cf9e616d265682cf88d057ee2d980d26e9b
Jenny Robbins <jenny.robbins@softwire.com>
2023-05-03 15:57:21 +0100

    try addign js

---
+++ script.js
@@ -0,0 +1,14 @@
+const API_KEY = hQZwgN6POGs7DTArUjjliXy2HNn5vtQm
+
+document.body.innerHTML="<p>hello</p>"
+
+// const numbersTable = document.getElementById("named-numbers-table")
+// numbersTable.addEventListener('click', function(event){
+//     document.body.innerHTML="<p>hello</p>"
+//     // // fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=number`).then(function (response) {
+//     //     const gif = response.data
+//     //     document.body.innerHTML = `<img src="${response.data.url}>`
+//     // });
+// })
+
+
